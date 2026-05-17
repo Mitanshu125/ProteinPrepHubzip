2b234c15b3b8908cf8c2db80ad1d019f011cfd95
Mitanshu125 <mitanshukhichi@gmail.com>
2026-05-17 19:27:16 +0530

    fix email transporter

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,12 +2,14 @@ import nodeMailer from "nodemailer";
 
 export const sendEmail = async (options) => {
  const transporter = nodeMailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.SMTP_MAIL,
-    pass: process.env.SMTP_PASSWORD,
-  },
-});
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    service: process.env.SMTP_SERVICE,
+    auth: {
+      user: process.env.SMTP_MAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
 
   const mailOptions = {
     from: process.env.SMTP_MAIL,
