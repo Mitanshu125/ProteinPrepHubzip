6c8ab2696fac53f3e6ab27104cdcb78664bb5ad1
Mitanshu125 <mitanshukhichi@gmail.com>
2026-05-18 15:26:41 +0530

    fix smtp port

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,10 +1,11 @@
 import nodeMailer from "nodemailer";
 
 export const sendEmail = async (options) => {
- const transporter = nodeMailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    service: process.env.SMTP_SERVICE,
+  const transporter = nodeMailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.SMTP_MAIL,
       pass: process.env.SMTP_PASSWORD,
@@ -15,7 +16,8 @@ export const sendEmail = async (options) => {
     from: process.env.SMTP_MAIL,
     to: options.email,
     subject: options.subject,
-    text: `${options.message} \n\nEmail of User Who Sent The Message: ${options.userEmail}`,
+    text: `${options.message} \n\nFrom: ${options.userEmail}`,
   };
+  
   await transporter.sendMail(mailOptions);
 };
